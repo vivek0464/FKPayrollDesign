10e4ec9cff57e6d2b279dccdf482aa047c12392e
vivek0464 <vivek.iitg0464@gmail.com>
2020-05-09 23:18:29 +0530

    Modified UnionMembership Class 	Added functions 		checkService(String service) 		getServiceCharges(String service) 		and getter, setter functions

--- src/com/flipkart/implementations/UnionMembership.java
+++ src/com/flipkart/implementations/UnionMembership.java
@@ -13,4 +13,40 @@ public class UnionMembership {
         this.duesRate = 0;
         this.serviceCharges = new HashMap<>();
     }
+
+    public boolean checkService(String service){
+        if(serviceCharges.containsKey(service)){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public void insertService(String service){
+        serviceCharges.put(service,0);
+    }
+
+    public void setServiceAndValue(String service, Integer value){
+        serviceCharges.put(service,value);
+    }
+
+    public void setMembership(boolean member) {
+        this.isMember = member;
+    }
+
+    public void setDuesRate(int duesRate) {
+        this.duesRate = duesRate;
+    }
+
+    public boolean isMember() {
+        return isMember;
+    }
+
+    public int getDuesRate() {
+        return duesRate;
+    }
+
+    public Integer getServiceCharges(String service) {
+        return serviceCharges.get(service);
+    }
 }
