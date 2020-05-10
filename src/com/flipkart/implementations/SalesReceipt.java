4797853234957cd0ef055ec2d124aaff73b06be9
vivek0464 <vivek.iitg0464@gmail.com>
2020-05-10 16:35:22 +0530

    Created Class SalesReceipt 	Added Functions 		getDate(), getAmount()

---
+++ src/com/flipkart/implementations/SalesReceipt.java
@@ -0,0 +1,21 @@
+package com.flipkart.implementations;
+
+import java.util.Calendar;
+
+public class SalesReceipt {
+    private Calendar date;
+    private double amount;
+
+    public SalesReceipt(Calendar date, double amount) {
+        this.date = date;
+        this.amount = amount;
+    }
+
+    public Calendar getDate() {
+        return date;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+}
