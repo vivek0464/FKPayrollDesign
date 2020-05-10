d76fd150953e5bde576aaa595751283da3b986c4
vivek0464 <vivek.iitg0464@gmail.com>
2020-05-10 14:58:30 +0530

    Added Class TimeCard 	Added functions getDate(), getTime()

---
+++ src/com/flipkart/implementations/TimeCard.java
@@ -0,0 +1,21 @@
+package com.flipkart.implementations;
+
+import java.util.Calendar;
+
+public class TimeCard {
+    private Calendar date;
+    private double hours;
+
+    public TimeCard(Calendar date, double hours) {
+        this.date = date;
+        this.hours = hours;
+    }
+
+    public Calendar getDate() {
+        return date;
+    }
+
+    public double getHours() {
+        return hours;
+    }
+}
